8bbae3c5be71f5658549c2559fd41363f85bbcda
황준용 <wnsdyd3333@gmail.com>
2024-03-21 17:39:53 +0900

    feat: 공통 컴포넌트 구현(버튼,헤더)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,13 @@
+import Button from '../components/Button';
+import Header from '../components/Header';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Header />
+      <Button text={123} />
+    </div>
+  );
 };
 
 export default Home;
